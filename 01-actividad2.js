e8ff8c49dcd08590a083f1f4adc73cd847df7b89
perezcleidy069-sketck <perezcleidy069@gmail.com>
2026-05-05 09:48:19 -0600

    feat: :tada: initial the project
    add the initial the activity in the html and css

---
+++ 01-actividad2.js
@@ -0,0 +1,16 @@
+const api="https://rickandmortyapi.com/api/character/"
+const contenido=document.getElementById("personajes")
+fetch(api)
+.then((response)=> response.json())
+.then((data)=>{
+    data.results.forEach((personaje) => {
+        const card=document.createElement("div");
+        card.innerHTML=
+        <h2>${personaje.name}</h2>
+        <img src="${personaje.image}" alt="${personaje.name}"></img>
+      
+
+        
+    });
+}
+)
