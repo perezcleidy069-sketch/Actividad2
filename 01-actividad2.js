8ab2b2882954d55a48bdd831d8f527bdd87f9b5d
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-05-06 16:37:06 -0600

    feat: :art: add the new fuction to search
    add the new fuction to add, this was created in js

--- 01-actividad2.js
+++ 01-actividad2.js
@@ -1,10 +1,13 @@
 const api="https://rickandmortyapi.com/api/character/"
 const contenido=document.getElementById("personajes")
 
-async function obtenerApi(api, contenido) {
+let nombresPersonajes=[]
+
+async function obtenerApi() {
     try{
         const respuesta= await fetch(api)
         const datos= await respuesta.json()
+        nombresPersonajes = datos.results.map(personaje => personaje.name);
         datos.results.forEach(personajes => {
             const card=document.createElement("div")
             card.className="person"
@@ -12,7 +15,6 @@ async function obtenerApi(api, contenido) {
             <h2>${personajes.name}</h2>
             <img src="${personajes.image}" alt="${personajes.name}">`
             ;contenido.appendChild(card)
-            
         });   
     }
     catch(error){
@@ -20,4 +22,30 @@ async function obtenerApi(api, contenido) {
     }
 }
 
-obtenerApi(api, contenido);
+obtenerApi();
+
+function buscar(){
+    let busco=document.getElementById("search").value.toLowerCase();
+    if (busco.trim() === "")return;
+    
+    let encontrar=[];
+    for (let i=0; i<nombresPersonajes.length; i++){
+        if(nombresPersonajes[i].toLowerCase().includes(busco)){
+            encontrar.push(nombresPersonajes[i]);
+        }
+    }
+    document.getElementById("personajes").innerHTML="";
+    if(encontrar.length >0){
+        for(let i = 0; i<encontrar.length; i++){
+            let dat=document.createElement("div");
+            dat.className="personss";
+            dat.textContent=encontrar[i];
+            document.getElementById("personajes").appendChild(dat)
+        }
+    }
+    else{
+        let date=document.createElement("div")
+        date.textContent="No se encuentra este personaje. Intentelo de nuevo" + busco;
+        document.getElementById("personajes").appendChild(date)
+    }
+}
