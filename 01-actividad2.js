2499800f98d15f8cd01d1281b126a3265569c94c
perezcleidy069-sketct <perezcleidy069@gmail.com>
2026-05-06 09:35:03 -0600

    feat: :art: improving the structure in js
    add to improve the structure of code in js

--- 01-actividad2.js
+++ 01-actividad2.js
@@ -1,15 +1,23 @@
 const api="https://rickandmortyapi.com/api/character/"
 const contenido=document.getElementById("personajes")
-fetch(api)
-.then((response)=> response.json())
-.then((data)=>{
-    data.results.forEach((personaje) => {
-        const card=document.createElement("div");
-        card.className = "personaje"
-        card.innerHTML=`
-        <h2>${personaje.name}</h2>
-        <img src="${personaje.image}" alt="${personaje.name}"></img>`
-        ;contenido.appendChild(card)
-    });
+
+async function obtenerApi(api, contenido) {
+    try{
+        const respuesta= await fetch(api)
+        const datos= await respuesta.json()
+        datos.results.forEach(personajes => {
+            const card=document.createElement("div")
+            card.className="person"
+            card.innerHTML=`
+            <h2>${personajes.name}</h2>
+            <img src="${personajes.image}" alt="${personajes.name}">`
+            ;contenido.appendChild(card)
+            
+        });   
+    }
+    catch(error){
+        console.log(error)
+    }
 }
-)
+
+obtenerApi(api, contenido);
