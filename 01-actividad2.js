03a9bb6dc688b1a4518d9975f736d891ffb262d1
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-05-09 23:29:54 -0600

    feat: :art: add new fuction like search and to improve and to create the flip card
    add to improve the code for creating the flip cards and searching

--- 01-actividad2.js
+++ 01-actividad2.js
@@ -1,20 +1,30 @@
 const api="https://rickandmortyapi.com/api/character/"
 const contenido=document.getElementById("personajes")
 
-let nombresPersonajes=[]
+//Para que colocar el api y se pueda ver en la pantalla y tambien para poder realizar el flip card utilize la misma 
 
 async function obtenerApi() {
     try{
         const respuesta= await fetch(api)
         const datos= await respuesta.json()
-        datos.results.forEach(personajes => {
-            const card=document.createElement("div")
-            card.className="person"
-            card.innerHTML=`
-            <h2>${personajes.name}</h2>
-            <img src="${personajes.image}" alt="${personajes.name}">`
-            ;contenido.appendChild(card)
-        });   
+        const personajes=datos.results.slice(0,20);
+        const resultado=personajes.map(a=>`
+            <div class="card">  
+                <div class="card__interior">
+                    <div class="persona">
+                        <h2><strong>Nombre:</strong>${a.name}</h2>
+                        <img src="${a.image}" alt="${a.name}">
+                    </div>
+                    <div class="cari">
+                        <p><strong>Estado: </strong>${a.status}</p>
+                        <p><strong>Especie: </strong>${a.species}</p>
+                        <p><strong>Género: </strong>${a.gender}</p>
+                        <p><strong>Origen: </strong>${a.origin.name}</p>
+                    </div>
+                </div>
+            </div>`
+        ).join('');
+        contenido.innerHTML = resultado; 
     }
     catch(error){
         console.log(error)
@@ -22,54 +32,54 @@ async function obtenerApi() {
 }
 obtenerApi();
 
+//Esto para buscar el pesonaje pero tambien para realizar realizar la animacion de espera
 
-async function segundoObtener(){
-    try{
-        const response= await fetch(api)
-        const data= await response.json()
-        data.results.forEach(a=>{
-            const cara=document.createElement("div")
-            cara.className="cari"
-            cara.innerHTML=`
-            <p>${a.status}</p>
-            <p>${a.species}</p>
-            <p>${a.gender}</p>
-            <p>${a.origin.name}`
-            contenido.appendChild(cara)
-        })
-    }
-    catch(erro){
-            console.log(error)
-    }
-}
-segundoObtener();
+async function buscarPersonajes(){
+    const inuput=document.getElementById("search");
+    const valorInput=inuput.value.trim().toLowerCase();
+    if(!valorInput)return
 
 
-
-
-
-function buscar(){
-    let busco=document.getElementById("search").value.toLowerCase();
-    if (busco.trim() === "")return;
+    //Esta parte es donde se realiza la animacion de espera
+    contenido.innerHTML=`
+        <div class="loader"></div>
+        <p class="cargando-texto">Buscando a ${valorInput}...</p> 
+    `;
+    const url=new URL(`https://rickandmortyapi.com/api/character/?name=${valorInput}`)
     
-    let encontrar=[];
-    for (let i=0; i<nombresPersonajes.length; i++){
-        if(nombresPersonajes[i].toLowerCase().includes(busco)){
-            encontrar.push(nombresPersonajes[i]);
+    try{
+        const res= await fetch(url);
+        const resulto= await res.json();
+        if(resulto.error){
+            contenido.innerHTML=`<p class="error">No se encontraron resultados para "${valorInput}"</p>`;
+            return;
         }
-    }
-    document.getElementById("personajes").innerHTML="";
-    if(encontrar.length >0){
-        for(let i = 0; i<encontrar.length; i++){
-            let dat=document.createElement("div");
-            dat.className="personss";
-            dat.textContent=encontrar[i];
-            document.getElementById("personajes").appendChild(dat)
-        }
-    }
-    else{
-        let date=document.createElement("div")
-        date.textContent="No se encuentra este personaje. Intentelo de nuevo" + busco;
-        document.getElementById("personajes").appendChild(date)
+        const busqueda=resulto.results.map(a=>`
+            <div class="card">  
+                <div class="card__interior">
+                    <div class="persona">
+                        <h2><strong>Nombre: </strong>${a.name}</h2>
+                        <img src="${a.image}" alt="${a.name}">
+                    </div>
+                    <div class="cari">
+                        <p><strong>Estado: </strong>${a.status}</p>
+                        <p><strong>Especie: </strong>${a.species}</p>
+                        <p><strong>Género: </strong>${a.gender}</p>
+                        <p><strong>Origen: </strong>${a.origin.name}</p>
+                    </div>
+                </div>
+            </div>`  
+        ).join('');
+        contenido.innerHTML=busqueda;
+    }catch(error){
+        console.log(error)
     }
 }
+document.querySelector("button").addEventListener("click", buscarPersonajes);
+    
+
+
+
+
+
+
